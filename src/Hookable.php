a718bd61318ba20b4f286a3d88e348486f57f83d
Jeremy Ward <jeremy@jmichaelward.com>
2022-02-05 10:26:35 -0600

    Add the Hookable interface.

---
+++ src/Hookable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace JMichaelWard\ContractsWP;
+
+/**
+ * Implement this interface on an object when that object contains callbacks for WordPress actions and filters.
+ *
+ * The benefit of this interfaces is that, generally, you can store all of the object's `add_action` and `add_filter`
+ * calls in a single location. This can make it easier to glance at an object and understand how it interacts with
+ * the WordPress application.
+ */
+interface Hookable {
+	/**
+	 * Register an object's hooks with WordPress.
+	 */
+	public function register_hooks(): void;
+}
